6a384e7779a1dc203adf166adb1f8cf23b5ea417
VaishaliA12 <avaishali96@gmail.com>
2025-02-20 21:36:24 +0530

    initial commit

---
+++ src/auth/sign-in/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignIn } from '@clerk/clerk-react'
+
+function SignInPage() {
+  return (
+    <div>
+    <SignIn/>
+    </div>
+  )
+}
+
+export default SignInPage 
